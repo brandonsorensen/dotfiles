0eb6a00f385aacf510865386e44552bcbaafdd76
Brandon Sorensen <brandon@brandonsorensen.com>
2024-03-28 21:23:55 +0100

    Pylsp config

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -147,7 +147,36 @@ require('lspconfig')['rust_analyzer'].setup {
 	capabilities = capabilities
 }
 require('lspconfig').rust_analyzer.setup({})
-require'lspconfig'.pylsp.setup{}
+require'lspconfig'.pylsp.setup {
+        settings = {
+      pylsp = {
+        plugins = {
+          -- formatter options
+          black = { enabled = false },
+          autopep8 = { enabled = false },
+          yapf = { enabled = false },
+          -- linter options
+          pylint = { enabled = false },
+          ruff = {
+			  enabled = true,
+			  select = {'ALL'}
+		  },
+          pyflakes = { enabled = false },
+          pycodestyle = { enabled = false },
+          -- type checker
+          pylsp_mypy = {
+            enabled = true,
+            report_progress = true,
+            live_mode = false
+          },
+          -- auto-completion options
+          jedi_completion = { enabled = true, fuzzy = true },
+          -- import sorting
+          isort = { enabled = false },
+        },
+      },
+    }
+}
 require('lspconfig').lua_ls.setup{
 	settings = {
 		Lua = {
