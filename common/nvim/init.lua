64cf92c7bdb9dd1894aa9094e24c7cbe0b1c2f8b
Brandon Sorensen <sorenb01@stepstone.com>
2024-05-07 16:29:23 +0200

    Remove comment lsp caps

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -203,11 +203,6 @@ cmp.setup.cmdline(':', {
 })
 
 -- Set up lspconfig.
--- local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
--- require('lspconfig')['rust_analyzer'].setup {
--- 	capabilities = capabilities
--- }
 require('lspconfig').rust_analyzer.setup {
 	settings = {
 		['rust-analyzer'] = {
