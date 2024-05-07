d76519b1ea1b4555c420b7f10d0ed11e15b530fd
Brandon Sorensen <sorenb01@stepstone.com>
2024-05-07 15:05:47 +0200

    Use rustaceanvim

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -156,14 +156,51 @@ cmp.setup.cmdline(':', {
 		{ name = 'cmdline' }
 	})
 })
-
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {
+  },
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+		checkOnSave = {
+			allFeatures = true,
+			overrideCommand = {
+				'cargo', 'clippy', '--workspace', '--message-format=json',
+				'--all-targets', '--all-features'
+			}
+		}
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {
+  },
+}
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['rust_analyzer'].setup {
-	capabilities = capabilities
-}
--- require('lspconfig').rust_analyzer.setup({})
+-- require('lspconfig').rust_analyzer.setup {
+-- 	capabilities = capabilities
+-- }
+-- require('lspconfig').rust_analyzer.setup {
+-- 	settings = {
+-- 		['rust-analyzer'] = {
+-- 			checkOnSave = {
+-- 				allFeatures = true,
+-- 				overrideCommand = {
+-- 					'cargo', 'clippy', '--workspace', '--message-format=json',
+-- 					'--all-targets', '--all-features'
+-- 				}
+-- 			}
+-- 		}
+-- 	}
+-- }
 require'lspconfig'.pylsp.setup {
 	settings = {
       pylsp = {
