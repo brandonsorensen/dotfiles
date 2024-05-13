e971cbc0cb01cbc77f4a8bae60c3fff2b5e0b2fe
Brandon Sorensen <sorenb01@stepstone.com>
2024-05-13 11:25:46 +0200

    Use filetype plugins

---
+++ common/nvim/after/ftplugin/typ.lua
@@ -0,0 +1,7 @@
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
+
+vim.g.typst_conceal = 1
+vim.g.typst_conceal_math = 1
+vim.g.typst_conceal_emoji = 1
+vim.g.typst_embedded_languages = {}
