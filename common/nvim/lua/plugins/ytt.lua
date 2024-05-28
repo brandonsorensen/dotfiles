dc85a7a6e67e76544ed3b6de1d56244c71b26d6c
Brandon Sorensen <sorenb01@stepstone.com>
2024-05-28 08:30:07 +0200

    Re-add YTT

---
+++ common/nvim/lua/plugins/ytt.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"cappyzawa/starlark.vim",
+		lazy = true
+	},
+	{
+		"carvel-dev/ytt.vim",
+		lazy = true,
+		dependencies = {
+			"starlark.vim"
+		},
+		keys = {
+			{
+				"<leader>ey", ":EnableYtt<CR>"
+			},
+			{
+				"<leader>dy", ":DisableYtt<CR>"
+			}
+		}
+	}
+}
