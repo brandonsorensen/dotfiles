fffc5ebc1609065dcc6a96b40be14787bbbfa1d3
Brandon Sorensen <brandon.sorensen@stepstone.com>
2025-01-01 10:50:27 -0800

    add nvim-lint

---
+++ common/nvim/lua/plugins/linting.lua
@@ -0,0 +1,20 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = {
+    "BufReadPre",
+    "BufNewFile",
+  },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      swift = { "swiftlint" },
+    }
+
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        lint.try_lint()
+      end,
+    })
+  end,
+}
