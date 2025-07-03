4a3ea881fe8e70d28fe4188cce28ea3c7b649259
Brandon Sorensen <brandon@brandonsorensen.com>
2025-07-03 20:03:11 +0200

    terra in config and tcc alias

--- common/.aliases.zsh
+++ common/.aliases.zsh
@@ -8,22 +8,5 @@ alias lg='lazygit'
 alias cb='cargo build'
 alias cr='cargo run'
 
-
-latex_aux_ext="*.log *.aux *.toc *.blg *.fdb_latexmk *.out *.fls *.synctex.gz *.bbl *.dvi"
-lf_args=$(echo $latex_aux_ext | sed -E 's/(\\)?(\*\.[a-z]+)/-I "\2"/g')
-
-alias cleantex='latexmk -C &> /dev/null'
-if [[ "$OSTYPE" = darwin* ]]; then
-	alias mvim='mvim -v'
-	# Always want color when using GNU ls
-	alias gls='gls --color=auto'
-	# Filters out the auxilary files that are output
-	# when compiling a LaTeX file; `lf` for list_filter
-	alias lf="gls $1 $lf_args"
-else
-	# On Linux I'll just use regular `ls` and hope the
-	# distro uses the GNU core
-	alias lf="ls $1 $lf_args"
-fi
-
 alias cpwd='pwd|pbcopy'
+alias tcc='tmux -CC'
