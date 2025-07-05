58aab4ac3547a23b9f37451efb11c848e2a33979
Brandon Sorensen <brandon@brandonsorensen.com>
2025-07-05 08:34:04 +0200

    add just bindings

--- common/.aliases.zsh
+++ common/.aliases.zsh
@@ -5,8 +5,14 @@ alias dtypes="column -xt $HOME/.dtypes.tsv"
 alias bztar="tar -jcvf"
 alias bzuntar="tar -xjf"
 alias lg='lazygit'
+
+# cargo
 alias cb='cargo build'
 alias cr='cargo run'
 
 alias cpwd='pwd|pbcopy'
 alias tcc='tmux -CC'
+
+# just
+alias jb='just build'
+alias jr='just run'
