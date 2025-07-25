b30c784f00e38dfe3522e956c52f5254e3f47aa9
Brandon Sorensen <brandon@brandonsorensen.com>
2025-07-25 14:06:33 +0200

    remove unused aliases

--- common/.aliases.zsh
+++ common/.aliases.zsh
@@ -2,19 +2,20 @@ alias la='ls -a'
 alias clock='date +%H:%M:%S'
 
 alias dtypes="column -xt $HOME/.dtypes.tsv"
-alias bztar="tar -jcvf"
-alias bzuntar="tar -xjf"
-alias lg='lazygit'
 
-# cargo
 alias cb='cargo build'
 alias cr='cargo run'
 
-alias cpwd='pwd|pbcopy'
 alias tcc='tmux -CC'
 
-# just
-alias jb='just build'
-alias jr='just run'
+alias cpwd='pwd|pbcopy'
 
 alias gs='git switch'
+alias co='git checkout'
+alias lg='lazygit'
+
+alias rg="rg --hidden --glob '!.git'"
+
+alias j='just'
+alias jb='just build'
+alias jr='just run'
