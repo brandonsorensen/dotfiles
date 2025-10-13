52efc1c9b786fa58f2ffdf1d34e038926d7a3e7d
Brandon Sorensen <brandon@brandonsorensen.com>
2025-10-13 09:53:34 +0200

    enable lsps

--- common/nvim/init.lua
+++ common/nvim/init.lua
@@ -15,4 +15,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.lsp.enable({
+  "rust_analyzer",
+  "lua_ls",
+  "bashls",
+  "just",
+  "basedpyright",
+  "ruff",
+  "terraformls",
+})
 require("lazy").setup("plugins")
