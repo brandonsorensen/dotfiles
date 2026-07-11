9ef92b557a42ee0169c1351ccadf0646475fac83
Brandon Sorensen <brandon@brandonsorensen.com>
2026-07-11 13:32:45 +0200

    Migrate nvim-treesitter to main branch

--- common/nvim/lua/plugins/treesitter.lua
+++ common/nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,31 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "rust",
-        "python",
-        "vim",
-        "vimdoc",
-        "lua",
-        "json",
-        "markdown",
-        "markdown_inline",
-        "hcl",
-        "terraform",
-      },
-      indent = { enable = true },
-      -- theme = 'nord',
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-      highlight = {
-        enable = true,
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+    -- markdown_inline has no filetype of its own; it's pulled in via injection
+    -- once the markdown parser is installed, so it's only needed in `install`.
+    require("nvim-treesitter").install({
+      "rust",
+      "python",
+      "vim",
+      "vimdoc",
+      "lua",
+      "json",
+      "markdown",
+      "markdown_inline",
+      "hcl",
+      "terraform",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      -- vimdoc's parser attaches to the `help` filetype
+      pattern = { "rust", "python", "vim", "help", "lua", "json", "markdown", "hcl", "terraform" },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
